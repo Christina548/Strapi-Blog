49c92c02681ac1d4b82ff8a7f4e83a27a0f87fc1
Christina <christina-hardy@live.nmit.ac.nz>
2021-10-19 09:23:33 +1300

    backend folder set up

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '823161d47a725d319eb307098cfde886'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bceaa323-375e-47e7-9e18-8c2d369a6b37'
+};
